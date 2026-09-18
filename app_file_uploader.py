8c5b2ba5a8a53e7c681e5c031185760e8bd309fc
Sparkling <853610889@qq.com>
2026-09-18 10:51:58 +0800

    third

--- app_file_uploader.py
+++ app_file_uploader.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from knowledge_base import knowlegdeBaseService
 st.title("知识库更新")
 
 uploader_file=st.file_uploader(
@@ -7,6 +8,9 @@ uploader_file=st.file_uploader(
     accept_multiple_files=False
 )
 
+service=knowlegdeBaseService()
+if "service" not in st.session_state:
+    st.session_state["service"]=knowlegdeBaseService()
 if uploader_file is not None:
     file_name=uploader_file.name
     file_type=uploader_file.type
@@ -14,5 +18,9 @@ if uploader_file is not None:
 
     st.subheader(f"文件名: {file_name}")
     st.write(f"格式: {file_type} | 大小: {file_size}")
+    text=uploader_file.getvalue().decode('utf-8')
 
+    with st.spinner("载入知识库中"):
+        res=st.session_state["service"].upload_by_str(text,file_name)
+        st.write(res)
    
